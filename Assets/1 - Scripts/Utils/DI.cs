4052092e6ccca8fa253068e2ef488614af2c7435
Vladislav Igorevich <vladislav.igorevich.workmail@gmail.com>
2023-04-03 14:14:50 +0300

    DI, Logger

---
+++ Assets/1 - Scripts/Utils/DI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DependencyInjection
+{
+    public static class DI
+    {
+        private static Dictionary<Type, object> injections;
+
+        private static Dictionary<Type, object> Injections =>
+            injections is null 
+            ? injections = new Dictionary<Type, object>() 
+            : injections;
+
+        public static void Add<T>(T obj)
+        {
+            if (!Injections.TryAdd(obj.GetType(), obj))
+            {
+                throw new Exception($"The object of type {typeof(T)} have been already added to the DI container");
+            }
+        }
+
+        public static T Get<T>()
+        {
+            var hasObject = Injections.TryGetValue(typeof(T), out object retVal);
+
+            if (!hasObject)
+            {
+                throw new Exception($"The object of type {typeof(T)} have not been added to the DI container");
+            }
+
+            return (T)retVal; 
+        }
+
+        public static bool Has<T>()
+        {
+            return Injections.ContainsKey(typeof(T));
+        }
+    }
+}
---
+++ Assets/1 - Scripts/Utils/Logger.cs
@@ -0,0 +1,45 @@
+﻿using DependencyInjection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Logger : MonoBehaviour
+{
+    [SerializeField] private Text logText;
+    [SerializeField] private Button clearButton;
+
+    private void Awake()
+    {
+        if (DI.Has<Logger>())
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(this);
+
+        clearButton.onClick.AddListener(Clear);
+
+        DI.Add(this);
+    }
+
+    private void Clear()
+    {
+        if (this == null || gameObject == null || logText == null) return;
+
+        logText.text = string.Empty;
+    }
+
+    public void Log<T>(T message)
+    {
+        if (this == null || gameObject == null || logText == null) return;
+
+        Debug.Log(message.ToString());
+
+        logText.text += message.ToString();
+        logText.text += "\n";
+    }
+
+    private void OnDestroy()
+    {
+        clearButton.onClick.RemoveListener(Clear);
+    }
+}
