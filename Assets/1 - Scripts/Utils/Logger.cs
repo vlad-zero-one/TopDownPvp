b7c19f4c1b38236a4563719bf6ad343638e1b75f
Vladislav Igorevich <vladislav.igorevich.workmail@gmail.com>
2023-04-11 06:55:18 +0300

    Simplified logger

--- Assets/1 - Scripts/Utils/Logger.cs
+++ Assets/1 - Scripts/Utils/Logger.cs
@@ -1,40 +1,17 @@
 ﻿using DependencyInjection;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Logger : MonoBehaviour
 {
-    [SerializeField] private Text logText;
-    [SerializeField] private Button clearButton;
-
     public void Init()
     {
         DontDestroyOnLoad(this);
-
-        clearButton.onClick.AddListener(Clear);
 
         DI.Add(this);
     }
 
     public void Log<T>(T message)
     {
-        if (this == null || gameObject == null || logText == null) return;
-
         Debug.Log(message.ToString());
-
-        logText.text += message.ToString();
-        logText.text += "\n";
-    }
-
-    private void Clear()
-    {
-        if (this == null || gameObject == null || logText == null) return;
-
-        logText.text = string.Empty;
-    }
-
-    private void OnDestroy()
-    {
-        clearButton.onClick.RemoveListener(Clear);
     }
 }
